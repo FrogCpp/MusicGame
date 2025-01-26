8a4bf4c7b0f8acad486b4fb18a5845671bf85ef2
SpAs3j9R <jekatopgg@gmail>
2025-01-26 19:53:14 +0300

    ya pomnyu penus bol`shoy

---
+++ music_analizer/music_main.py
@@ -0,0 +1,10 @@
+import librosa
+
+
+y, sr = librosa.load("/Users/spas6j9r/Library/Application Support/Steam/steamapps/music/Dying Light Original Soundtrack/Dying Light Original Soundtrack/02. Horizon.mp3")
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+print(tempo)
+print(beat_times)
+# tempo — удары в минуту
+# beat_times — таймкоды битов
