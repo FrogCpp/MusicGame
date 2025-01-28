6d9c93fa7612b564f34c21fa5aef0e9f2bae4631
SpAs3j9R <jekatopgg@gmail>
2025-01-28 17:18:43 +0300

    ya pomnyu penus bol`shoy

--- music_analizer/music_main.py
+++ music_analizer/music_main.py
@@ -5,6 +5,8 @@ y, sr = librosa.load("/Users/spas6j9r/Library/Application Support/Steam/steamapp
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 print(tempo)
-print(beat_times)
+print(beat_times, end='\n\n\n\n')
+print(type(beat_frames))
 # tempo — удары в минуту
 # beat_times — таймкоды битов
+
